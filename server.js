fa33beb4f9233a231bb1ca7193a4ccfd18c72516
María José Marra <majomarra@gmail.com>
2020-12-03 12:20:40 -0300

    updated server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const APP_PORT = process.env.APP_PORT || 5000;
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
-const cors = require('cors');
-const seeds = require('./seeds/seeds');
-
-app.use(cors()); // Use this after the variable declaration
+const cors = require("cors");
+const seeds = require("./seeds/seeds");
 
 // seeds();
 
@@ -17,9 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-require('./routes/routes')(app);
+app.use(cors()); // Use this after the variable declaration
+
+require("./routes/routes")(app);
 
 app.listen(APP_PORT, () => {
-  console.log(`Listening on port ${APP_PORT}`);
-  console.log(`Enter http://localhost:${APP_PORT}`);
+	console.log(`Listening on port ${APP_PORT}`);
+	console.log(`Enter http://localhost:${APP_PORT}`);
 });
